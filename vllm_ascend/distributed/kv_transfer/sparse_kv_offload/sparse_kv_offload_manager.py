2470e78c7f39511884b861d6eb18d4c41e13c9e6
wangyuzheng <yuuzhwang@gmail.com>
2026-07-29 20:20:23 +0800

    Respect configured compiler for sparse KV offload build

--- vllm_ascend/distributed/kv_transfer/sparse_kv_offload/sparse_kv_offload_manager.py
+++ vllm_ascend/distributed/kv_transfer/sparse_kv_offload/sparse_kv_offload_manager.py
@@ -314,8 +314,16 @@ class SparseKVOffloadManager:
         torch_npu_include = os.path.join(torch_npu_path, "include")
         torch_npu_lib_path = os.path.join(torch_npu_path, "lib")
         os.environ["TORCH_EXTENSIONS_ALWAYS_BUILD"] = "1"
-        os.environ['CXX'] = 'clang++'
-        os.environ['CC'] = 'clang'
+        os.environ["CXX"] = (
+            os.environ.get("VLLM_ASCEND_SFA_CXX")
+            or os.environ.get("CXX")
+            or "clang++"
+        )
+        os.environ["CC"] = (
+            os.environ.get("VLLM_ASCEND_SFA_CC")
+            or os.environ.get("CC")
+            or "clang"
+        )
         abs_path = os.path.dirname(os.path.abspath(__file__))
         src_path = os.path.join(abs_path, "sparse_kv_offload.cpp")
         logger.info(f'Sparse KV offload build cpp utils from src: {src_path}')
